a0fa2a288c837d8d4ab3a457726d3f91664d5c29
jsmith@conflexion.com <jsmith@conflexion.com>
2019-02-21 06:50:12 -0600

    Initial entry into the repository
    R and Python code for regression.

---
+++ R/regression.R
@@ -0,0 +1,51 @@
+#
+# Regression
+#
+# 1/25/2019 - Jeff Smith
+#
+
+library(tidyverse)
+
+#
+# Cereals - From Larose and Larose
+#
+cereals <- read_csv("../data/cereals.csv", col_names = TRUE)
+ggplot(data=cereals) +
+  geom_point(aes(x=Sugars, y=Rating))
+(r <- lm(Rating~Sugars, cereals))
+print(summary(r))
+
+# use lm to add the regression line
+ggplot(data=cereals,aes(x=Sugars, y=Rating)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE)
+
+# manually add the line using the slope/intercept
+ggplot(data=cereals) +
+  geom_point(aes(x=Sugars, y=Rating)) +
+  geom_abline(aes(slope=r$coefficients[2],intercept=r$coefficients[1]),color='red')
+
+
+#
+# Data from https://newonlinecourses.science.psu.edu/stat462/node/101/
+#
+# Poverty
+poverty <- read_csv("../data/teen_birthrate_poverty.csv", col_names = TRUE)
+ggplot(data=poverty, aes(x=PovPct, y=Brth15to17)) +
+  geom_point() +
+  geom_smooth(method="lm", se = FALSE)
+(r <- lm(Brth15to17~PovPct,poverty))
+print(summary(r))
+
+#
+# Lung function
+lung_function <- read_csv("../data/lung_function.csv", col_names = TRUE)
+ggplot(data=lung_function) +
+  geom_point(aes(x=age, y=FEV))
+(r <- lm(FEV~age,lung_function))
+print(summary(r))
+
+
+# Multiple regression
+(r <- lm(Rating~Sugars+Fiber, cereals))
+print(summary(r))
